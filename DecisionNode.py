4d6e14f87845517d68eca3112b94ccadbb2d2c6d
Tharak2009Coder <Tharak@health-sathi.org>
2026-08-05 20:33:31 -0400

    Delete DecisionNode.py

--- DecisionNode.py
+++
@@ -1,8 +0,0 @@
-
-class DecisionNode:
-    
-    def __init__(self, condition=None, yes=None, no=None, result=None):
-        self.condition = condition
-        self.yes = yes
-        self.no = no
-        self.result = result
